92c1b6907c41c7db3c716a6d3ef66d244c500d1d
Arne Dußin <arne.dussin@live.de>
2019-10-13 19:15:44 +0200

    rustfmt

--- src/player/card_transferer.rs
+++ src/player/card_transferer.rs
@@ -1,5 +1,3 @@
 pub trait CardTransferer {
-    fn can_transfer(pos: (u8, u8), other_pos: (u8, u8)) -> bool {
-        pos == other_pos
-    }
+    fn can_transfer(pos: (u8, u8), other_pos: (u8, u8)) -> bool { pos == other_pos }
 }
--- src/player/courier.rs
+++ src/player/courier.rs
@@ -1,7 +1,11 @@
-pub struct Courier;
+use crate::math::Vec2;
+use crate::player::CardTransferer;
+
+pub struct Courier {
+    name: String,
+    pos:  Vec2<u8>
+}
 
 impl CardTransferer for Courier {
-    fn can_transfer(_: (u8, u8), _: (u8, u8)) -> bool {
-        true
-    }
+    fn can_transfer(_: (u8, u8), _: (u8, u8)) -> bool { true }
 }
