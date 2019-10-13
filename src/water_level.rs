351f41188519ca260475bd8d6f196d1613b9744e
Arne Dußin <arne.dussin@live.de>
2019-10-13 19:16:36 +0200

    Begin implementing WaterLevel

--- src/water_level.rs
+++ src/water_level.rs
@@ -1,20 +1,27 @@
 use std::ops::{Add, AddAssign};
+use std::u8;
+
+use crate::difficulty::Difficulty;
 
 pub const MAX_WATER_LEVEL: u8 = 9;
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct WaterLevel {
     level: u8
 }
 
 impl WaterLevel {
-    pub fn new(level: u8) -> WaterLevel {
-        WaterLevel {
-            level
-        }
-    }
+    pub fn new(level: u8) -> Self { Self { level } }
 
-    pub fn from_difficulty(difficulty: Difficulty) -> WaterLevel {
-        unimplemented!()
+    pub fn from_difficulty(difficulty: Difficulty) -> Self {
+        let level = match difficulty {
+            Difficulty::Novice => 0,
+            Difficulty::Normal => 1,
+            Difficulty::Elite => 2,
+            Difficulty::Legendary => 3
+        };
+
+        Self::new(level)
     }
 
     pub fn draw_amount(&self) -> u8 {
@@ -23,21 +30,17 @@ impl WaterLevel {
             2..=4 => 3,
             5 | 6 => 4,
             7 | 8 => 5,
-            _ => u8::max()
+            _ => u8::MAX
         }
     }
 }
 
 impl Add<u8> for WaterLevel {
-    type Output = WaterLevel;
+    type Output = Self;
 
-    fn add(&self, other: u8) -> Output {
-        WaterLevel::new(self.level + other)
-    }
+    fn add(self, other: u8) -> Self::Output { Self::new(self.level + other) }
 }
 
 impl AddAssign<u8> for WaterLevel {
-    fn add_assign(&mut self, other: u8) {
-        self.level += other;
-    }
+    fn add_assign(&mut self, other: u8) { self.level += other; }
 }
