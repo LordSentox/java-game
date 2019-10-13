385039979a5df24e2567d7a306d8821dfd516d12
Arne Dußin <arne.dussin@live.de>
2019-10-13 19:15:03 +0200

    Rename ArtefactCardStack to CardStack
    The ArtefactCardStack and FloodCardStack are so similar, that they don't really need special Implementations.

---
+++ src/card_stack.rs
@@ -0,0 +1,16 @@
+pub struct CardStack<T> {
+    draw_stack:    Vec<T>,
+    discard_stack: Vec<T>
+}
+
+impl<T> CardStack<T> {
+    pub fn new() -> Self { unimplemented!() }
+
+    pub fn discard_stack(&self) -> &Vec<T> { &self.discard_stack }
+
+    pub fn draw_card(&mut self) -> T { unimplemented!() }
+
+    pub fn discard_card(&mut self, card: T) { unimplemented!() }
+
+    pub fn shuffle_back(&mut self) { unimplemented!() }
+}
