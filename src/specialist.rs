f9ba1cffa51abf2f28bb883bfc959d6cce3a29df
Arne Dußin <arne.dussin@live.de>
2019-10-15 18:51:28 +0200

    Add documentation to SpecialistInfo

--- src/specialist.rs
+++ src/specialist.rs
@@ -43,7 +43,22 @@ pub trait Specialist: SpecialistInfo {
     }
 }
 
+/// Information about the specialist, that is known and true no matter the
+/// current action state, but not necessarily known at compile time.
 pub trait SpecialistInfo: Positionable {
+    /// Returns true, if the player does not have to do anything to activate the
+    /// special ability of the adventurer. This does not mean the
+    /// special_moves function should be omitted when querying for all
+    /// movement options.
+    // TODO: This should be moved to the adventurer type enum, since it is known at
+    // TODO  compile time.
+    fn implicit_special() -> bool { false }
+
+    /// The normal movement set the adventurer can do. This usually does not
+    /// have to be overwritten, since the normal movements should be the
+    /// same.
+    // TODO: This should be in the character, since it does not change depending on
+    // TODO  the adventurer anymore.
     fn moves(&self, map: &FullMap) -> Vec<Vec2<u8>> {
         self.pos()
             .neighbours(map.limit_rect())
@@ -52,10 +67,17 @@ pub trait SpecialistInfo: Positionable {
             .collect()
     }
 
+    /// The special move set of the adventurer. If the adventurer has a special
+    /// ability that is not concerned with movement, the standard
+    /// imlpmentation will suffice.
     fn special_moves(&self, map: &FullMap) -> Vec<Vec2<u8>> { Vec::new() }
 
+    /// Returns if the player is in principle capable of moving others.
+    // TODO: This should be moved to the adventurer type enum, since it is known at
+    // TODO  compile time.
     fn can_move_others() -> bool { false }
 
+    /// The position set the adventurer can drain from their current position.
     fn drains(&self, map: &FullMap) -> Vec<Vec2<u8>> {
         let mut positions = self.pos().neighbours(map.limit_rect());
         positions.push(self.pos());
@@ -65,5 +87,8 @@ pub trait SpecialistInfo: Positionable {
             .collect()
     }
 
+    // TODO: This might have to be rethought, because of the way one can trade cards
+    // TODO  if one has too many. In that case the position or adventurer type does
+    // TODO  not matter.
     fn can_transfer_cards<T: Positionable>(&self, other: &T) -> bool { self.pos() == other.pos() }
 }
