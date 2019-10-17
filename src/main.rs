18b7d108187f4ed0008b5a7b1bdac6cec84f6ea5
Arne Dußin <arne.dussin@live.de>
2019-10-17 11:17:32 +0200

    Add crate level doc and make mods pub
    Mods had to be made public, so that their documentation is exposed and
    one can actually browse them instead of just having a blank page.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 #![allow(dead_code)]
 
+//! # \[J\]ust \[a\]nother \[v\]irtual \[a\]dventure game
+//!
+//! Originating from the Software Praktikum at TU Dortmund and written in Java,
+//! this is a Rust rewrite of the computer game based on the board game "The
+//! Forbidden Island". It is purely for educational purposes and fun.
+
 #[macro_use]
 extern crate java_game_derive;
 
-mod action_state;
-mod adventurer;
-mod card_stack;
-mod character;
-mod difficulty;
-mod direction;
-mod map;
-mod math;
-mod positionable;
-mod state;
-mod system;
-mod water_level;
+pub mod action_state;
+pub mod adventurer;
+pub mod card_stack;
+pub mod character;
+pub mod difficulty;
+pub mod direction;
+pub mod map;
+pub mod math;
+pub mod positionable;
+pub mod state;
+pub mod system;
+pub mod water_level;
 
 fn main() {}
