01c9fa67726c3f58e00177b5c84ed8a8b9bb6019
Arne Dußin <arne.dussin@live.de>
2019-10-23 16:58:34 +0200

    Implement deref to internal adventurer for representing character

--- src/character.rs
+++ src/character.rs
@@ -1,8 +1,25 @@
 use amethyst::ecs::{Component, DenseVecStorage};
 
 use crate::adventurer::Adventurer;
+use std::ops::{Deref, DerefMut};
 
+/// A character is the playable instance of one player.
+///
+/// # Associated Components
+/// [Transform](amethyst::core::Transform) - The position of the character on
+/// the screen. Automatically updated by the
+/// [CharacterTransformUpdate](crate::system::CharacterTransformUpdate) system.
 #[derive(Component)]
 pub struct Character {
     internal_handler: Box<dyn Adventurer>
+}
+
+impl Deref for Character {
+    type Target = dyn Adventurer;
+
+    fn deref(&self) -> &Self::Target { self.internal_handler.as_ref() }
+}
+
+impl DerefMut for Character {
+    fn deref_mut(&mut self) -> &mut Self::Target { self.internal_handler.as_mut() }
 }
