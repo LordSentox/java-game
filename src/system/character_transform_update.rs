8c7c41f9ae9f74cea6b5edae992b2a5cb8c2182f
Arne Dußin <arne.dussin@live.de>
2019-10-23 16:57:58 +0200

    Add stub for CharacterTransformUpdate system

--- src/system/character_transform_update.rs
+++ src/system/character_transform_update.rs
@@ -1,1 +1,12 @@
+use amethyst::ecs::System;
 
+/// Responsible for updating the transformations of the player characters based
+/// on the current position of them and the information available of the map
+/// they are playing on.
+pub struct CharacterTransformUpdate {}
+
+impl<'a> System<'a> for CharacterTransformUpdate {
+    type SystemData = ();
+
+    fn run(&mut self, mut _characters: Self::SystemData) { unimplemented!() }
+}
