88b9b7b6b105d757fff45e353d52008c221c1121
Arne Dußin <arne.dussin@live.de>
2019-10-28 16:36:43 +0100

    Implement adventurer for engineer

--- src/adventurer/engineer.rs
+++ src/adventurer/engineer.rs
@@ -4,13 +4,14 @@
 //! The engineer can shore up two tiles for one action point, unless they
 //! perform a movement action in between those two drains.
 
-use crate::positionable::Positionable;
-
 use super::{Adventurer, AdventurerInfo};
+use crate::map::Full as MapFull;
+use crate::positionable::Positionable;
 
 #[derive(Positionable)]
 pub struct Engineer {
-    pos: FieldPos
+    pos: FieldPos,
+    extra_drain: bool
 }
 
 impl Engineer {
@@ -22,12 +23,32 @@ impl Engineer {
 impl Adventurer for Engineer {
     /// If the engineer moves, they may not drain a second tile without spending
     /// an extra action point.
-    fn on_move(&mut self) { unimplemented!() }
+    fn on_move(&mut self) { self.extra_drain = false; }
+
+    /// Instead of just checking the action points, this function must also
+    /// check if there is an extra drain, in which case the action points
+    /// can be ignored.
+    fn drains(&self, map: &MapFull, act_points: u8) -> Vec<FieldPos> {
+        if act_points != 0 || self.extra_drain {
+            AdventurerInfo::drains(self, &map)
+        }
+        else {
+            Vec::new()
+        }
+    }
 
     /// Instead of always costing an action point. If for the last drain action
     /// an action point was already consumed, the action points remain
     /// unchanged.
-    fn on_drain(&mut self, _act_points: &mut u8) { unimplemented!() }
+    fn on_drain(&mut self, act_points: &mut u8) {
+        if self.extra_drain {
+            self.extra_drain = false;
+        }
+        else {
+            *act_points -= 1;
+            self.extra_drain = true;
+        }
+    }
 }
 
 impl AdventurerInfo for Engineer {}
