1e94682246f0e69ddb0bc2b088472cccd31bdad1
Arne Dußin <arne.dussin@live.de>
2019-10-28 17:03:20 +0100

    Implement adventurer for pilot

--- src/adventurer/pilot.rs
+++ src/adventurer/pilot.rs
@@ -6,7 +6,7 @@
 //! with them.
 
 use super::{Adventurer, AdventurerInfo};
-use crate::map::Full as FullMap;
+use crate::map::{Full as MapFull, MapExt};
 use crate::positionable::Positionable;
 
 #[derive(Positionable)]
@@ -25,5 +25,16 @@ impl Adventurer for Pilot {}
 impl AdventurerInfo for Pilot {
     /// The pilot may fly anywhere on the map where they can stand when they use
     /// their special ability.
-    fn special_moves(&self, _map: &FullMap) -> Vec<FieldPos> { unimplemented!() }
+    fn special_moves(&self, map: &MapFull) -> Vec<FieldPos> {
+        map.iter()
+            .filter_map(|(pos, _)| {
+                if map.is_standable(pos) {
+                    Some(pos)
+                }
+                else {
+                    None
+                }
+            })
+            .collect()
+    }
 }
