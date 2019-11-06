c270d99f386b7dc4d282ed59510c3101c5e4010e
Arne Dußin <arne.dussin@live.de>
2019-11-06 02:04:55 +0100

    Add test for default adventurer movements

--- src/adventurer/adventurer.rs
+++ src/adventurer/adventurer.rs
@@ -1,7 +1,7 @@
 //! Traits that describe the actions an Adventurer can take that may be unique
 //! to them.
 
-use crate::map::{Full as FullMap, MapExt};
+use crate::map::{Full as MapFull, MapExt};
 use crate::math::Vec2;
 use crate::positionable::Positionable;
 
@@ -18,7 +18,7 @@ pub trait Adventurer: AdventurerInfo + Send + Sync {
     /// their turn.
     fn can_act(&self, act_points: u8) -> bool { act_points != 0 }
 
-    fn moves(&self, map: &FullMap, act_points: u8) -> Vec<Vec2<u8>> {
+    fn moves(&self, map: &MapFull, act_points: u8) -> Vec<Vec2<u8>> {
         if act_points != 0 {
             AdventurerInfo::moves(self, &map)
         }
@@ -27,7 +27,7 @@ pub trait Adventurer: AdventurerInfo + Send + Sync {
         }
     }
 
-    fn special_moves(&self, _map: &FullMap, _act_points: u8) -> Vec<Vec2<u8>> { unimplemented!() }
+    fn special_moves(&self, _map: &MapFull, _act_points: u8) -> Vec<Vec2<u8>> { unimplemented!() }
 
     /// If something special should happen, when the adventurer moves, this
     /// needs to be implemented. Does not get the action points, since none
@@ -41,7 +41,7 @@ pub trait Adventurer: AdventurerInfo + Send + Sync {
     /// Navigator, which has to handle their extra push.
     fn on_move_other(&mut self, _act_points: &mut u8) {}
 
-    fn drains(&self, map: &FullMap, act_points: u8) -> Vec<Vec2<u8>> {
+    fn drains(&self, map: &MapFull, act_points: u8) -> Vec<Vec2<u8>> {
         if act_points != 0 {
             AdventurerInfo::drains(self, &map)
         }
@@ -73,7 +73,7 @@ pub trait AdventurerInfo: Positionable {
     /// same.
     // TODO: This should be in the character, since it does not change depending on
     // TODO  the adventurer anymore.
-    fn moves(&self, map: &FullMap) -> Vec<Vec2<u8>> {
+    fn moves(&self, map: &MapFull) -> Vec<Vec2<u8>> {
         self.pos()
             .neighbours(Some(map.limit_rect()))
             .into_iter()
@@ -84,10 +84,10 @@ pub trait AdventurerInfo: Positionable {
     /// The special move set of the adventurer. If the adventurer has a special
     /// ability that is not concerned with movement, the standard
     /// implementation will suffice.
-    fn special_moves(&self, _map: &FullMap) -> Vec<Vec2<u8>> { Vec::new() }
+    fn special_moves(&self, _map: &MapFull) -> Vec<Vec2<u8>> { Vec::new() }
 
     /// The position set the adventurer can drain from their current position.
-    fn drains(&self, map: &FullMap) -> Vec<Vec2<u8>> {
+    fn drains(&self, map: &MapFull) -> Vec<Vec2<u8>> {
         let mut positions = self.pos().neighbours(Some(map.limit_rect()));
         positions.push(self.pos());
         positions
@@ -98,4 +98,60 @@ pub trait AdventurerInfo: Positionable {
 
     /// Returns if the Adventurer can trade cards to the one provided as other.
     fn can_trade_with(&self, other: &dyn Positionable) -> bool { self.pos() == other.pos() }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::map::{IslandTile, IslandTileInfo, IslandTileState};
+
+    // Boring adventurer without any special abilities
+    #[derive(Positionable)]
+    struct CaptainAwesome {
+        pos: FieldPos
+    }
+
+    impl Adventurer for CaptainAwesome {}
+    impl AdventurerInfo for CaptainAwesome {}
+
+    fn map_all_moveable(size: Vec2<u8>) -> MapFull {
+        let dry = IslandTile::new(IslandTileInfo::CoralPalace);
+        let mut flooded = dry.clone();
+        flooded.set_state(IslandTileState::Flooded);
+
+        let mut tiles = vec![Vec::with_capacity(size.x as usize); size.y as usize];
+        for y in 0..size.y {
+            for x in 0..size.x {
+                tiles[y as usize].push(if (x + y) % 2 == 0 {
+                    Some(dry.clone())
+                }
+                else {
+                    Some(flooded.clone())
+                });
+            }
+        }
+
+        tiles.into()
+    }
+
+    #[test]
+    fn default_moves_enough_act_points() {
+        let map = map_all_moveable(Vec2::from_values(5, 5));
+        let adventurer = CaptainAwesome {
+            pos: FieldPos::from_values(2, 2)
+        };
+
+        let mut expected_moves = vec![
+            Vec2::from_values(2, 1),
+            Vec2::from_values(2, 3),
+            Vec2::from_values(1, 2),
+            Vec2::from_values(3, 2),
+        ];
+        let mut actual_moves = Adventurer::moves(&adventurer, &map, 3);
+
+        expected_moves.sort();
+        actual_moves.sort();
+
+        assert_eq!(expected_moves, actual_moves);
+    }
 }
