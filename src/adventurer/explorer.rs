f3cbc8f826cc14a87423791b80790e3fdfc75976
Arne Dußin <arne.dussin@live.de>
2019-11-06 23:34:51 +0100

    Add test for explorer special move set

--- src/adventurer/explorer.rs
+++ src/adventurer/explorer.rs
@@ -37,3 +37,32 @@ impl AdventurerInfo for Explorer {
             .collect()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::map::{IslandTile, IslandTileInfo, IslandTileState};
+    use crate::math::Vec2;
+
+    #[test]
+    fn special_moves() {
+        let dry = IslandTile::new(IslandTileInfo::LostLagoon);
+        let mut gone = IslandTile::new(IslandTileInfo::GoldGate);
+        gone.set_state(IslandTileState::Gone);
+
+        let mut map = MapFull::new(Vec2::from_values(5, 5), Some(dry));
+        map.set(Vec2::from_values(1, 1), Some(gone.clone()));
+        map.set(Vec2::from_values(1, 3), None);
+
+        let mut explorer = Explorer::new();
+        explorer.set_pos(Vec2::from_values(2, 2));
+
+        let mut expected_moves = vec![Vec2::from_values(3, 1), Vec2::from_values(3, 3)];
+        let mut actual_moves = AdventurerInfo::special_moves(&explorer, &map);
+
+        expected_moves.sort();
+        actual_moves.sort();
+
+        assert_eq!(expected_moves, actual_moves);
+    }
+}
