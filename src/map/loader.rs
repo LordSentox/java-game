f750105025ea9eeeef4ace5da325150365c4e025
Arne Dußin <arne.dussin@live.de>
2019-11-11 12:26:33 +0100

    rustfmt

--- src/map/loader.rs
+++ src/map/loader.rs
@@ -4,12 +4,13 @@ use std::io::Error;
 use super::full::Full;
 
 /// Load a map from a file
-/// 
+///
 /// # Parameters
 /// `path` - The path of the map file
-/// 
+///
 /// # Returns
-/// Ok<Map> if the map could be loaded, Err<Error> containing an IO error otherwise
+/// Ok<Map> if the map could be loaded, Err<Error> containing an IO error
+/// otherwise
 pub fn load_map(path: String) -> Result<Full, Error> {
     let file_str = &fs::read_to_string(path)?;
 
@@ -19,13 +20,14 @@ pub fn load_map(path: String) -> Result<Full, Error> {
 }
 
 /// Save a map from to file
-/// 
+///
 /// # Parameters
 /// * `path` - The path of the map file
 /// * `map` - The map to be saved
-/// 
+///
 /// # Returns
-/// `Ok<())>` if the map could be saved, `Err<Error>` containing an IO error otherwise
+/// `Ok<())>` if the map could be saved, `Err<Error>` containing an IO error
+/// otherwise
 pub fn save_map(path: String, map: &Full) -> Result<(), Error> {
     let serialized = serde_json::to_string(map)?;
 
@@ -49,8 +51,9 @@ mod tests {
 
         // Save and load map
         save_map(String::from("test_save_load_map.maptmp"), &full).expect("Could not save the map");
-        
-        let full_loaded = load_map(String::from("test_save_load_map.maptmp")).expect("Could not load the map");
+
+        let full_loaded =
+            load_map(String::from("test_save_load_map.maptmp")).expect("Could not load the map");
 
         // Make sure that original map and saved/loaded map are the same
         assert_eq!(full, full_loaded);
